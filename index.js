2329a279bec6650f5b6a65d2827df8eca8cde921
Steve Cresswell <229672+cressie176@users.noreply.github.com>
2024-12-19 03:25:11 +0000

    Update eslint

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 module.exports = {
-    real: function() {
-        return {
-            now: function() {
-                return Date.now()
-            }
-        }
-    },
-    fake: function() {
+  real() {
+    return {
+      now() {
+        return Date.now();
+      },
+    };
+  },
+  fake() {
 
-        var time = new Date('2016-02-02T11:00:00.000Z').getTime()
+    let time = new Date('2016-02-02T11:00:00.000Z').getTime();
 
-        return {
-            now: function() {
-                return time
-            },
-            fix: function(value) {
-                time = new Date(value).getTime()
-                if (isNaN(time)) throw new Error('Invalid Date: ' + value)
-                return this
-            }
-        }
-    }
-}
+    return {
+      now() {
+        return time;
+      },
+      fix(value) {
+        time = new Date(value).getTime();
+        if (Number.isNaN(time)) throw new Error(`Invalid Date: ${value}`);
+        return this;
+      },
+    };
+  },
+};
--- test/index.tests.js
+++ test/index.tests.js
@@ -1,30 +1,30 @@
-var assert = require('assert')
-var clock = require('..')
+const assert = require('assert');
+const clock = require('..');
 
-describe('Fake Clock', function() {
+describe('Fake Clock', () => {
 
-    it('should default to groundhog day', function() {
-        assert.equal(clock.fake().now(), 1454410800000)
-    })
+  it('should default to groundhog day', () => {
+    assert.equal(clock.fake().now(), 1454410800000);
+  });
 
-    it('should fixed time', function() {
-        assert.equal(clock.fake().fix(1469563181761).now(), 1469563181761)
-        assert.equal(clock.fake().fix(new Date(1469563181761)).now(), 1469563181761)
-        assert.equal(clock.fake().fix(new Date(1469563181761).toISOString()).now(), 1469563181761)
-    })
+  it('should fixed time', () => {
+    assert.equal(clock.fake().fix(1469563181761).now(), 1469563181761);
+    assert.equal(clock.fake().fix(new Date(1469563181761)).now(), 1469563181761);
+    assert.equal(clock.fake().fix(new Date(1469563181761).toISOString()).now(), 1469563181761);
+  });
 
-    it('should reject attempts to fix time to an invalid value', function() {
-        assert.throws(function() {
-            clock.fake().fix("foo")
-        }, /Invalid Date: foo/)
-    })
+  it('should reject attempts to fix time to an invalid value', () => {
+    assert.throws(() => {
+      clock.fake().fix('foo');
+    }, /Invalid Date: foo/);
+  });
 
-})
+});
 
-describe('Real Clock', function() {
+describe('Real Clock', () => {
 
-    it('should use correct time', function() {
-        assert.ok(Math.abs(clock.real().now() - Date.now()) < 100)
-    })
+  it('should use correct time', () => {
+    assert.ok(Math.abs(clock.real().now() - Date.now()) < 100);
+  });
 
-})
+});
